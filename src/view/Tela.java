7d7d75675f58eba2884328f9c85bf9198b4d025a
Lucas Augusto <luckaugusto@gmail.com>
2016-04-01 01:13:14 -0300

    Att Tela

--- src/view/Tela.java
+++ src/view/Tela.java
@@ -3,14 +3,31 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class Tela extends JFrame {
 
-	private JPanel contentPane;
-
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel screenPanel;
+	private JComboBox CbBxEscola;
+	private JComboBox CbBxJurado;
+	private JComboBox CbBxQuesito;
+	private JTextField txtNota;
+	private JButton btnInserir;
+	private JButton btnQuesito;
+	private JButton btnTotal;
+	private JLabel lblEscola;
+	private JLabel lblJurado;
+	private JLabel lblQuesito;
+	private JLabel lblNota;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -33,10 +50,10 @@ public class Tela extends JFrame {
 	public Tela() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		screenPanel = new JPanel();
+		screenPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		screenPanel.setLayout(new BorderLayout(0, 0));
+		setContentPane(screenPanel);
 	}
 
 }
