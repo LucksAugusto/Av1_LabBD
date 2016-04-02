a197e4b43859b79f262036adfb4f258912f4ef67
Lucas Augusto <luckaugusto@gmail.com>
2016-04-02 01:27:31 -0300

    Criação de classes e termino do layout Tela

--- src/view/Tela.java
+++ src/view/Tela.java
@@ -9,9 +9,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 
 public class Tela extends JFrame {
+	private String[] escolaList = {"Unidos da vila maria","Rio tiete"};
+	private String[] juradoList = {"Zé","Zó"};
+	private String[] quesitoList = {"Bunda","Peito"};
 
 	private static final long serialVersionUID = 1L;
 	
@@ -48,12 +53,67 @@ public class Tela extends JFrame {
 	 * Create the frame.
 	 */
 	public Tela() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		setTitle("Apuração... Nota... 10");
+		setType(Type.UTILITY);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 400);
+		setLocationRelativeTo(null);
 		screenPanel = new JPanel();
 		screenPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		screenPanel.setLayout(new BorderLayout(0, 0));
+		screenPanel.setLayout(null);
 		setContentPane(screenPanel);
+		
+		lblEscola = new JLabel("Escola: ");
+		lblEscola.setBounds(50, 20, 100, 25);
+		CbBxEscola = new JComboBox(escolaList);
+		CbBxEscola.setBounds(100, 20, 450, 25);
+		
+		lblJurado = new JLabel("Jurado: ");
+		lblJurado.setBounds(50, 60, 100, 25);
+		CbBxJurado = new JComboBox(juradoList);
+		CbBxJurado.setBounds(100, 60, 450, 25);
+		
+		lblQuesito = new JLabel("Quesito: ");
+		lblQuesito.setBounds(50, 100, 100, 25);
+		CbBxQuesito = new JComboBox(quesitoList);
+		CbBxQuesito.setBounds(100, 100, 350, 25);
+		
+		lblNota = new JLabel("Nota: ");
+		lblNota.setBounds(50, 250, 100, 25);
+		txtNota = new JTextField(15);
+		txtNota.setBounds(100, 250, 100, 25);
+		btnInserir = new JButton("Inserir");
+		btnInserir.setBounds(200, 250, 100, 25);
+		
+		btnQuesito = new JButton("Ver Quesitos");
+		btnQuesito.setBounds(200, 300, 100, 25);
+		btnTotal = new JButton("Ver Total");
+		btnTotal.setBounds(320, 300, 100, 25);
+		
+		
+		screenPanel.add(lblEscola);
+		screenPanel.add(CbBxEscola);
+		screenPanel.add(lblJurado);
+		screenPanel.add(CbBxJurado);
+		screenPanel.add(lblQuesito);
+		screenPanel.add(CbBxQuesito);
+		screenPanel.add(lblNota);
+		screenPanel.add(txtNota);
+		screenPanel.add(btnInserir);
+		screenPanel.add(btnQuesito);
+		screenPanel.add(btnTotal);
 	}
 
 }
